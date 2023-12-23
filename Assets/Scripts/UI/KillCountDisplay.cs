9e1cb213929e68cc0ffd0c29ab87306de3138dea
WatabeShi <shintarouwatabe2022@gmail.com>
2023-12-23 18:10:52 +0900

    Fix killcount
    ・撃破数の表示の修正

--- Assets/Scripts/UI/KillCountDisplay.cs
+++ Assets/Scripts/UI/KillCountDisplay.cs
@@ -18,6 +18,6 @@ public class KillCountDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        killCounter.text = "Œ‚”j” : " + gameManager.killCount;
+        killCounter.text = "Œ‚”j” : " + gameManager.killCount + " / " + gameManager.ClearConditions;
     }
 }
