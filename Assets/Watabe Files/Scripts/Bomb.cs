4c08d3f4d6345bc580415841efe1e59bc2f621d8
WatabeShi <shintarouwatabe2022@gmail.com>
2024-01-23 12:52:58 +0900

    爆弾改造２
    クイックボム

--- Assets/Watabe Files/Scripts/Bomb.cs
+++ Assets/Watabe Files/Scripts/Bomb.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
@@ -19,6 +20,10 @@ public class Bomb : MonoBehaviour
     [Header("爆発SE"), SerializeField]
     private AudioClip explodeSE;
 
+    private IEnumerator explodeCoru = null;
+
+    private bool isExplode = false;   // 爆発したかどうか
+
     private AudioSource audioSource;
 
     private void Awake()
@@ -35,7 +40,10 @@ public class Bomb : MonoBehaviour
         if (explodeRange <= 0) explodeRange = 1;
 
         sphereCollider.enabled = false;
-        Invoke("Begine", explodeDelay);
+        //Invoke("Begine", explodeDelay);
+
+        explodeCoru = Explosion(true);
+        StartCoroutine(explodeCoru);
     }
 
     // Update is called once per frame
@@ -44,21 +52,32 @@ public class Bomb : MonoBehaviour
         Explode();
     }
 
-    private void Begine()   // 起爆処理
+    private IEnumerator Explosion(bool isStart)
     {
-        sphereCollider.enabled = true;
-
-        GetComponent<MeshRenderer>().enabled = false;   // 爆弾を消す（見た目だけ）
-
-        if (explodeEffect != null)
+        if(isStart)
         {
-            GameObject _explode = Instantiate(explodeEffect, transform.position, Quaternion.identity);   // 爆発エフェクト生成
-            _explode.transform.localScale *= explodeRange / 2.5f;   // エフェクトのサイズ調整
+            sphereCollider.enabled = false;
+
+            yield return new WaitForSeconds(explodeDelay);
         }
 
-        audioSource.PlayOneShot(explodeSE);   // 爆発SE再生
+        if(!isExplode)
+        {
+            isExplode = true;
+            GetComponent<MeshRenderer>().enabled = false;   // 爆弾を消す（見た目だけ）
+            sphereCollider.enabled = true;
 
-        Invoke("DestroyMe", 5);
+            if (explodeEffect != null)
+            {
+                GameObject _explode = Instantiate(explodeEffect, transform.position, Quaternion.identity);   // 爆発エフェクト生成
+                _explode.transform.localScale *= explodeRange / 2.5f;   // エフェクトのサイズ調整
+            }
+            audioSource.PlayOneShot(explodeSE);   // 爆発SE再生
+        }
+
+        yield return new WaitForSeconds(5);
+
+        Destroy(this.gameObject);
     }
 
     private void Explode()   // 爆破範囲の更新処理
@@ -76,17 +95,13 @@ public class Bomb : MonoBehaviour
         }
     }
 
-    private void DestroyMe()
-    {
-        Destroy(this.gameObject);
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("SnowMan"))
+        if (collision.gameObject.CompareTag("SnowMan") && !isExplode)
         {
-            Begine();
-            sphereCollider.enabled = true;
+            explodeCoru = null;
+            explodeCoru = Explosion(false);
+            StartCoroutine(explodeCoru);
         }
     }
 }
