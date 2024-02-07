18b13436fbabc3f155fdb05649a737bf9cae6cab
カシワンギ <gaburiera.co.sg@gmail.com>
2024-02-07 11:55:02 +0900

    Lifeスクリプト改造

--- Assets/Scripts/Player/LifeControl.cs
+++ Assets/Scripts/Player/LifeControl.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeControl : MonoBehaviour
 {
     [Header("プレイヤーライフ"), SerializeField]
     private int lifePoint = 3;
+    private Image DamageImage;
 
     public void Damage()
     {
         lifePoint--;
         print(lifePoint);
+        DamageImage.gameObject.SetActive(false);
+        Invoke("DamageImage", 0.1f);
     }
 
     public int Life
